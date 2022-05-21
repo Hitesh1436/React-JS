ea7ee6df5025100b70223a2d14b74d6e4526fd9b
Hitesh1436 <hsc253545@gmail.com>
2022-05-21 20:42:34 +0530

    first Day

---
+++ reels/src/secrets.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgxxdcpFwsPXxO9psZlaHRYnByZHHUUnE",
+    authDomain: "reels-auth-2082f.firebaseapp.com",
+    projectId: "reels-auth-2082f",
+    storageBucket: "reels-auth-2082f.appspot.com",
+    messagingSenderId: "337680328398",
+    appId: "1:337680328398:web:20787a118aeeac75c4129f"
+  };
+
+  module.exports = firebaseConfig;
