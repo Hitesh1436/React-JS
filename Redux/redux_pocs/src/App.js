c2330f06bb779c163f70a3ecd38e9153c8e5fb22
Hitesh1436 <hsc253545@gmail.com>
2022-06-05 20:53:58 +0530

    useReducer ka use

---
+++ Redux/redux_pocs/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Ball from './components/Ball';
+import Bat from './components/Bat';
+import BatuseReducer from './components/BatuseReducer';
+
+function App() {
+  return (
+    <>
+    <Ball/>
+    <Bat/>
+    <BatuseReducer/>
+    </>
+  );
+}
+
+export default App;
